2fb0a70925d091b5d16f8f0385f9c74e98c9c7c3
’rinasuzuki52’ <rinasuzuki1015@gmail.com>
2025-08-12 09:48:50 +0900

    カテゴリー文字数制限削除

--- js/main.js
+++ js/main.js
@@ -122,22 +122,6 @@ $(document).ready(function(){
 
 
 
-// 5文字まで表示
-$(function(){
-  // 表示文字数の上限（ここでは25文字に設定）
-  const LIMIT = 5;
-
-  // 対象となるセレクタをカンマでつなぐ
-  $('js-ellipsis5').each(function(){
-    const txt = $(this).text().trim();
-    if (txt.length > LIMIT) {
-      // substr の第1引数に0, 第2引数にLIMIT を指定
-      $(this).text( txt.substr(0, LIMIT) + '…' );
-    }
-  });
-});
-
-
 // 20文字まで表示
 $(function(){
   // 表示文字数の上限（ここでは25文字に設定）
