173ed307174ad3ea90a3a29a1dff681279fce7f1
rinasuzuki52 <rinasuzuki1015@gmail.com>
2025-11-11 09:41:49 +0900

    Merge branch 'develop001'

--- front-page.php
+++ front-page.php
@@ -122,23 +122,8 @@
       生徒さんたちの声
     </h2>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div class="p-voice__slider p-voice-slider swiper">
       <div class="p-voice-slider__cards swiper-wrapper">
-=======
-    <div class="swiper p-voice__slider p-voice-slider">
-      <div class="swiper-wrapper">
->>>>>>> 1309562 (swiperへ変更)
-=======
-    <div class="p-voice__slider p-voice-slider swiper">
-      <div class="p-voice-slider__cards swiper-wrapper">
->>>>>>> ea33edd (swiper修正)
-=======
-    <div class="p-voice__slider p-voice-slider swiper">
-      <div class="p-voice-slider__cards swiper-wrapper">
->>>>>>> develop001
         <?php
         $args = array(
           'post_type' => 'result',
@@ -148,23 +133,8 @@
         if ($the_query->have_posts()) :
           while ($the_query->have_posts()) : $the_query->the_post();
         ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         <div class="p-voice-slider__card p-slider-card swiper-slide">
           <a href="<?php the_permalink(); ?>">
-=======
-        <div class="swiper-slide">
-          <a href="<?php the_permalink(); ?>" class="p-voice-slider__card p-slider-card">
->>>>>>> 1309562 (swiperへ変更)
-=======
-        <div class="p-voice-slider__card p-slider-card swiper-slide">
-          <a href="<?php the_permalink(); ?>">
->>>>>>> ea33edd (swiper修正)
-=======
-        <div class="p-voice-slider__card p-slider-card swiper-slide">
-          <a href="<?php the_permalink(); ?>">
->>>>>>> develop001
             <div class="p-slider-card__img">
               <?php if (has_post_thumbnail()) : ?>
                 <?php the_post_thumbnail(); ?>
